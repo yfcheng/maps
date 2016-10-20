d9ad0b55fa665d1dcd0cb7dc5d72240287c622de
yfcheng <yfcheng0@gmail.com>
2016-10-19 22:30:19 -0700

    Update myMap.js

--- myMap.js
+++ myMap.js
@@ -45,7 +45,7 @@ function get_pokemon_layer_from_map_items(map_items) {
 
 function add_pokemon_layer() {
     var pokemon_layer =  get_pokemon_layer_from_map_items(map_manager.map_items);
-    map.layers.insert(pokemon_layer); 
+    map_manager.map.layers.insert(pokemon_layer); 
 }
 // 3. Add pokemon count down refresh
 
